5e7ab682cb92fe362be5e76e35f99be7f35f656c
jaemin <jaemin4033@gmail.com>
2024-06-11 15:43:55 +0900

    LazyLoadingImage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,380 @@
-import logo from './logo.svg';
 import './App.css';
+import LazyLoadingImage from './components/card/LazyLoadingImage';
 
 function App() {
+
+  const posterUrl1 = "https://d1h3jce690ir5k.cloudfront.net/images/poster/141945/141945.jpg"
+  const posterUrl2 = "https://d1h3jce690ir5k.cloudfront.net/images/poster/114894/114894.jpg"
+  const posterUrl3 = "https://d1h3jce690ir5k.cloudfront.net/images/poster/140302/140302.jpg"
+  const posterUrl4 = "https://d1h3jce690ir5k.cloudfront.net/images/poster/128225/128225.jpg"
+  const posterUrl5 = "https://d1h3jce690ir5k.cloudfront.net/images/poster/126940/126940.jpg"
+  const posterUrl6 = "https://d1h3jce690ir5k.cloudfront.net/images/poster/141671/141671.jpg"
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl1}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl2}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl3}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl4}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl5}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl6}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl1}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl2}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl3}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl4}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl5}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl6}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl1}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl2}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl3}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl4}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl5}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl6}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl1}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl2}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl3}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl4}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl5}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl6}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl1}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl2}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl3}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl4}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl5}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl6}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl1}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl2}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl3}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl4}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl5}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl6}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl1}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl2}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl3}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl4}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl5}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl6}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl1}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl2}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl3}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl4}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl5}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl6}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl1}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl2}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl3}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl4}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl5}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl6}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl1}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl2}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl3}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl4}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl5}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl6}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl1}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl2}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl3}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl4}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl5}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl6}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl1}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl2}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl3}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl4}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl5}/>
+            </div>
+          </li>
+          <li>
+            <div style={{width:"300px",height:"400px",background:"green"}}>
+              <LazyLoadingImage imgSrc={posterUrl6}/>
+            </div>
+          </li>
+        </ul>
       </header>
     </div>
   );
---
+++ src/components/card/LazyLoadingImage.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState, useRef } from 'react'
+import { ImageInfo } from './LazyLoadingImage.types'
+
+
+
+
+
+const options: IntersectionObserverInit = {
+  rootMargin: "60000px"
+}
+
+const LazyLoadingImage = ({ imgSrc }: ImageInfo) => {
+  const imgRef = useRef(null);
+  const [imageSrc,setImageSrc] = useState("")
+
+  const callback = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+    entries.forEach(entry =>{
+      if(entry.isIntersecting){
+        console.log("화면 들어옴")
+        setImageSrc(imgSrc)
+        observer.unobserve(imgRef.current!)
+      }
+    })
+  }
+  useEffect(()=>{
+
+    const observer = new IntersectionObserver(callback, options)
+    if(imgRef.current){
+      observer.observe(imgRef.current)
+    }
+
+    return()=>{
+      if(imgRef.current){
+        observer.unobserve(imgRef.current)
+      }
+    }
+  },[])
+  
+
+
+  return (
+    <img ref={imgRef} width={"300px"} height={"auto"} src={imageSrc}/>
+  )
+}
+
+export default LazyLoadingImage
